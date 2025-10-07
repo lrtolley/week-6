aea339ea5a681d8060ef4f80eaabd80069a12d47
Lauren T <ltolley@iu.edu>
2025-10-07 10:43:11 -0400

    adding the .env file with API keys

--- apputil.py
+++ apputil.py
@@ -1,1 +1,9 @@
-# your code here ...
+from dotenv import load_dotenv
+import os
+
+ACCESS_TOKEN = os.environ['wkKOf5ZVIQKtx5QRpgUoRHo9iUTaMxh4ghmZsUS-KyP-gij_5WYKZXrhTa3_RvFS']
+
+load_dotenv()
+
+search_term = "Missy Elliott"
+genius_search_url = f"http://api.genius.com/search?q={search_term}&access_token={ACCESS_TOKEN}"
